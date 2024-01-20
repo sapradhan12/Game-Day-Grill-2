d36a4e7d5d4f0011a9c525405090210cbb7ca9a4
dan-nguyen1004 <dmnguyen@bluevalleyk12.net>
2024-01-20 12:46:21 -0800

    beginning of home page

---
+++ Common/common.js
@@ -0,0 +1,10 @@
+function dropDown(){
+    document.getElementById("before").style.display = "none";
+    document.getElementById("after").style.display = "block";
+    document.getElementById("container").style.display = "block";
+}
+function goUp(){
+    document.getElementById("before").style.display = "block";
+    document.getElementById("after").style.display = "none";
+    document.getElementById("container").style.display = "none";
+}
